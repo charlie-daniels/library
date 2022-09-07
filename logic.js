8761d8d9ad3714c939ea3fb72d5bd6a7ed8a9755
charlie-daniels <charlied030401@gmail.com>
2022-09-07 15:32:40 +0100

    Add basic book logic

--- logic.js
+++ logic.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -10,3 +12,23 @@ function Book(title, author, pages, isRead) {
         return info += 'read';
     };
 }
+
+function addBookToLibrary(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
+}
+
+function displayBooks() {
+  const container = document.querySelector('.books');
+  myLibrary.map((b, index) => {
+    const newTile = document.createElement('div');
+    newTile.classList.add('book', `data-index=${index}`);
+
+    const bookInfo = document.createElement('p');
+    bookInfo.textContent = b.info();
+    const t = newTile.appendChild(bookInfo)
+    
+    container.appendChild(newTile);
+  });
+}
+
